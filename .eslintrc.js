d3f5473590eb7793147fe891a8b61648a84f893d
gomezzzer <gomezgeorge132@gmail.com>
2024-08-14 19:46:35 -0500

    simple-import-sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,10 @@ module.exports = {
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier', 'simple-import-sort'],
- 
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],  
+    'no-console': 'off',
+    'simple-import-sort/imports': 'error',
+  },
 };
 

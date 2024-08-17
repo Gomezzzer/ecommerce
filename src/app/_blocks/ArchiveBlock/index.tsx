09e5ab291197417c8aefd6e49b2199dc0d2fe307
gomezzzer <gomezgeorge132@gmail.com>
2024-08-17 18:55:59 -0500

    archhive error

--- src/app/_blocks/ArchiveBlock/index.tsx
+++ src/app/_blocks/ArchiveBlock/index.tsx
@@ -36,7 +36,7 @@ export const ArchiveBlock: React.FC<
         relationTo={relationTo}
         populatedDocs={populatedDocs}
         populatedDocsTotal={populatedDocsTotal}
-        selectedDocs={selectedDocs}
+       // selectedDocs={selectedDocs}
         categories={categories}
         limit={limit}
         sort="-publishedOn"

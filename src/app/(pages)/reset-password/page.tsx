2193d57bc85b925a57f8139984414f1c52c415b0
gomezzzer <gomezgeorge132@gmail.com>
2024-08-20 11:53:00 -0500

    passsword error

--- src/app/(pages)/reset-password/page.tsx
+++ src/app/(pages)/reset-password/page.tsx
@@ -1,27 +1,20 @@
-import React from 'react';
-import { Metadata } from 'next';
-import { useRouter } from 'next/router';
+import React from 'react'
+import { Metadata } from 'next'
 
-import { Gutter } from '../../_components/Gutter';
-import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph';
-import { ResetPasswordForm } from './ResetPasswordForm';
+import { Gutter } from '../../_components/Gutter'
+import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
+import { ResetPasswordForm } from './ResetPasswordForm'
 
-import classes from './index.module.scss';
+import classes from './index.module.scss'
 
-export default function ResetPassword() {
-  const router = useRouter();
-
-  if (router.isFallback) {
-    return <h1>Loading...</h1>; // Display a loading state while the page is being generated
-  }
-
+export default async function ResetPassword() {
   return (
     <Gutter className={classes.resetPassword}>
       <h1>Reset Password</h1>
       <p>Please enter a new password below.</p>
       <ResetPasswordForm />
     </Gutter>
-  );
+  )
 }
 
 export const metadata: Metadata = {
@@ -31,4 +24,4 @@ export const metadata: Metadata = {
     title: 'Reset Password',
     url: '/reset-password',
   }),
-};
+}

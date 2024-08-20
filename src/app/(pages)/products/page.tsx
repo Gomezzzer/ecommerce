b2aaa110831cf36572d83f3aefa4944b37614af0
gomezzzer <gomezgeorge132@gmail.com>
2024-08-20 11:47:44 -0500

    password error

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -41,3 +41,6 @@ const Products = async () => {
 }
 
 export default Products
+
+
+
--- src/app/(pages)/reset-password/page.tsx
+++ src/app/(pages)/reset-password/page.tsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import { Metadata } from 'next'
+import React from 'react';
+import { Metadata } from 'next';
+import { useRouter } from 'next/router';
 
-import { Gutter } from '../../_components/Gutter'
-import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
-import { ResetPasswordForm } from './ResetPasswordForm'
+import { Gutter } from '../../_components/Gutter';
+import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph';
+import { ResetPasswordForm } from './ResetPasswordForm';
 
-import classes from './index.module.scss'
+import classes from './index.module.scss';
 
-export default async function ResetPassword() {
+export default function ResetPassword() {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h1>Loading...</h1>; // Display a loading state while the page is being generated
+  }
+
   return (
     <Gutter className={classes.resetPassword}>
       <h1>Reset Password</h1>
       <p>Please enter a new password below.</p>
       <ResetPasswordForm />
     </Gutter>
-  )
+  );
 }
 
 export const metadata: Metadata = {
@@ -24,4 +31,4 @@ export const metadata: Metadata = {
     title: 'Reset Password',
     url: '/reset-password',
   }),
-}
+};
